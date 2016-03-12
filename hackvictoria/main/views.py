b2ce32d59a3317a43a6d0209f91e2ef404d948e2
aleksiy325 <aleksiy123@gmail.com>
2016-03-12 15:55:12 -0800

    added login

--- hackvictoria/main/views.py
+++ hackvictoria/main/views.py
@@ -1,25 +1,49 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect, HttpResponse
 from main.forms import UserForm, UserProfileForm
 from django.template import RequestContext
 from django.shortcuts import render_to_response
+from django.contrib.auth import authenticate
 # Create your views here.
 
 def mainpage(request):
+    #FIX TO REDIRECT
     context = RequestContext(request)
-
     registered = False
-
-    if request == 'POST':
+    if request.method == 'POST':
+        print "POST"
         user_form = UserForm(data=request.POST)
-
         if user_form.is_valid():
-            user =user_form.save()
+            user = user_form.save()
             user.set_password(user.password)
             user.save()
+            registered = True
         else:
             print user_form.errors
     else:
         user_form = UserForm()
 
     return render_to_response('main/main.html',{'user_form': user_form, 'registered': registered}, context)
+
+def login(request):
+    context = RequestContext(request)
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+
+        if user:
+            if user.is_active:
+                login(request, user)
+                #LOGIN
+                return HttpResponseRedirect('/userpage/')
+
+            else:
+                return HttpResponse("Account is inactive")
+        else:
+            print "Invalid login details: {0}, {1}".format(username, password)
+            #FIX this?
+            return HttpResponse("Invalid login.")
+
+    else:
+        return render_to_response('main/login.html', {}, context)
