7a87f27acd152e0e61f0a90433a65b0e7c942076
aleksiy325 <aleksiy123@gmail.com>
2016-03-12 15:08:14 -0800

    added registration

--- hackvictoria/main/models.py
+++ hackvictoria/main/models.py
@@ -1,5 +1,24 @@
 from __future__ import unicode_literals
-
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+
+    #TODO
+    #picture = models.ImageField(upload_to='profile_images', blank=True)
+
+
+     # Override the __unicode__() method to return out something meaningful!
+    def __unicode__(self):
+        return self.user.username
+
+
+
+
+
+
+
+
--- hackvictoria/main/views.py
+++ hackvictoria/main/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from main.forms import UserForm, UserProfileForm
+from django.template import RequestContext
+from django.shortcuts import render_to_response
 # Create your views here.
 
 def mainpage(request):
-    return render(request, 'main/main.html')
+    context = RequestContext(request)
+
+    registered = False
+
+    if request == 'POST':
+        user_form = UserForm(data=request.POST)
+
+        if user_form.is_valid():
+            user =user_form.save()
+            user.set_password(user.password)
+            user.save()
+        else:
+            print user_form.errors
+    else:
+        user_form = UserForm()
+
+    return render_to_response('main/main.html',{'user_form': user_form, 'registered': registered}, context)
